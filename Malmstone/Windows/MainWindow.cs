26a0ade115f6d12cd67d8bcc2f31d367a5e70fee
Pinapelz <yukais@pinapelz.com>
2025-08-26 11:16:42 -0700

    fix: remove duplicate % sign from frontline bonus tracking

--- Malmstone/Windows/MainWindow.cs
+++ Malmstone/Windows/MainWindow.cs
@@ -150,12 +150,12 @@ namespace Malmstone.Windows
                         {
                             if(Plugin.PvPService.ConsecutiveThirdPlaceFrontline == 1)
                             {
-                                ImGui.TextColored(new Vector4(0.0f, 1.0f, 0.0f, 1.0f), "Primed For Bonus: You'll receive a 10%% reward bonus if you place 3rd");
+                                ImGui.TextColored(new Vector4(0.0f, 1.0f, 0.0f, 1.0f), "Primed For Bonus: You'll receive a 10% reward bonus if you place 3rd");
                             }
                             if (ImGui.IsItemHovered())
                             {
                                 ImGui.BeginTooltip();
-                                ImGui.Text("You're primed for a reward bonus! You will get a 10%% reward bonus if you place 3rd again" +
+                                ImGui.Text("You're primed for a reward bonus! You will get a 10% reward bonus if you place 3rd again" +
                                     "\nCounter resets if you rank 1st");
                                 ImGui.EndTooltip();
                             }
@@ -164,9 +164,9 @@ namespace Malmstone.Windows
                         else
                         {
                             if (Plugin.PvPService.CurrentFrontlineLosingBonus != 50)
-                                ImGui.TextColored(new Vector4(0.0f, 1.0f, 0.0f, 1.0f), "You'll receive a " + Plugin.PvPService.CurrentFrontlineLosingBonus + "%% reward bonus after placing 1st or 2nd");
+                                ImGui.TextColored(new Vector4(0.0f, 1.0f, 0.0f, 1.0f), "You'll receive a " + Plugin.PvPService.CurrentFrontlineLosingBonus + "% reward bonus after placing 1st or 2nd");
                             else
-                                ImGui.TextColored(new Vector4(0.0f, 1.0f, 0.0f, 1.0f), "You'll receive a " + Plugin.PvPService.CurrentFrontlineLosingBonus + "%% reward bonus after placing 1st, 2nd, or 3rd");
+                                ImGui.TextColored(new Vector4(0.0f, 1.0f, 0.0f, 1.0f), "You'll receive a " + Plugin.PvPService.CurrentFrontlineLosingBonus + "% reward bonus after placing 1st, 2nd, or 3rd");
                             if (ImGui.IsItemHovered())
                             {
                                 ImGui.BeginTooltip();
@@ -176,11 +176,11 @@ namespace Malmstone.Windows
                             }
                             if (Plugin.PvPService.CurrentFrontlineLosingBonus != 50)
                             {
-                                ImGui.TextColored(new Vector4(0.0f, 1.0f, 0.0f, 1.0f), "Your reward bonus will increase to " + (Plugin.PvPService.CurrentFrontlineLosingBonus + 10) + "%% if you place 3rd");
+                                ImGui.TextColored(new Vector4(0.0f, 1.0f, 0.0f, 1.0f), "Your reward bonus will increase to " + (Plugin.PvPService.CurrentFrontlineLosingBonus + 10) + "% if you place 3rd");
                                 if (ImGui.IsItemHovered())
                                 {
                                     ImGui.BeginTooltip();
-                                    ImGui.Text($"Finishing 3rd again will increase your bonus to {Plugin.PvPService.CurrentFrontlineLosingBonus + 10}%%" +
+                                    ImGui.Text($"Finishing 3rd again will increase your bonus to {Plugin.PvPService.CurrentFrontlineLosingBonus + 10}%" +
                                                    "\nThis increased bonus will also apply to the match where this happens");
                                     ImGui.EndTooltip();
                                 }
